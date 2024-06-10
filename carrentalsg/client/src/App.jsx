8b498c9c64749ceab92a7e40bc62d464a77647b8
Au Hui Qi, Jodie <2102418@sit.singaporetech.edu.sg>
2024-06-10 21:21:08 +0800

    Created Basic Routes
     Added basic routes like login, about and sign up , referencing #3

--- carrentalsg/client/src/App.jsx
+++ carrentalsg/client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import Logout from './pages/Logout';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+//import Userprofile from './pages/Userprofile';
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter> 
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
 }
