a0b861c7eab669ae196e9188ade453b4f56a5d84
Au Hui Qi, Jodie <2102418@sit.singaporetech.edu.sg>
2024-06-22 17:06:41 +0800

    create test route
    test route, controller and user route  #8

--- carrentalsg/api/index.js
+++ carrentalsg/api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotnev from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotnev.config();
 
 mongoose.connect(process.env.MONGO).then (() => {
@@ -14,3 +15,5 @@ app.listen(3000,() => {
     console.log('Server is running on port 3000');
 }
 );
+
+app.use("/api/user",userRouter); 
