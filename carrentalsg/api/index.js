976448b94539167eec07adf76e5b361d76c03227
Au Hui Qi, Jodie <2102418@sit.singaporetech.edu.sg>
2024-06-22 15:49:30 +0800

    create mongo connection
    created mongo connection #6

--- carrentalsg/api/index.js
+++ carrentalsg/api/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotnev from 'dotenv';
+dotnev.config();
 
+mongoose.connect(process.env.MONGO).then (() => {
+    console.log('Connected to MongoDB!');
+ }).catch((err) => {
+    console.log(err);
+ })
 const app = express();
 
 app.listen(3000,() => {
