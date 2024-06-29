416bb03d5d323a9a01b5c7d5ba9df2944148e4bc
Au Hui Qi, Jodie <2102418@sit.singaporetech.edu.sg>
2024-06-29 22:26:03 +0800

    API Route for sign up

---
+++ carrentalsg/api/controller/auth.controller.js
@@ -0,0 +1,18 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+export const signup = async (req,res) => {
+
+//save in db
+const {username,email,password} = req.body;
+const hashedPassword=bcryptjs.hashSync(password,10); // encryption
+const newUser = new User ({username,email,password:hashedPassword});
+await newUser.save()
+
+try{
+res.status(201).json("User created successfully!");
+
+}
+catch(error){
+res.status(500).json(error.message)
+}
+};
