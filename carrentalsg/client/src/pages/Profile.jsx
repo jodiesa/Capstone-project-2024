7ad6723dde459928829cb83a8143731a54fb8757
Au Hui Qi, Jodie <2102418@sit.singaporetech.edu.sg>
2024-10-15 21:04:23 +0800

    update user API
    #20

--- carrentalsg/client/src/pages/Profile.jsx
+++ carrentalsg/client/src/pages/Profile.jsx
@@ -1,24 +1,79 @@
 import React from 'react'
 import { useRef, useState, useEffect } from 'react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
 import { useSelector } from 'react-redux';
-
-
+import { updateUserStart,updateUserFailure,updateUserSuccess } from '../../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 export default function Profile() {
-  const {currentUser}=useSelector((state) => state.user)
+
+  const fileRef = useRef(null);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
+  const dispatch = useDispatch();
+
+  
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const [ formData,setFormData] = useState({});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
+
   return (
 <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col'>
+      <form onSubmit={handleSubmit} className='flex flex-col'>
         <img src={currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
-        <input type="text" placeholder ='username' className = 'border p-3 rounded-lg' id='username'/>
-        <input type="text" placeholder ='password' className = 'border p-3 rounded-lg' id='password'/>
-        <input type="text" placeholder ='email' className = 'border p-3 rounded-lg' id='email'/>
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>update</button>
+        <input type="text" placeholder ='username' defaultValue={currentUser.username} className = 'border p-3 rounded-lg' id='username' onChange={handleChange} />
+        <input type="password" placeholder ='password' defaultValue={currentUser.password} className = 'border p-3 rounded-lg' id='password' onChange={handleChange}  />
+        <input type="text" placeholder ='email' defaultValue={currentUser.email} className = 'border p-3 rounded-lg' id='email' onChange={handleChange}/>
+        <button 
+         disabled={loading}
+        className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button>
         </form>
         <div className="flex justify-between mt-5">
           <span className='text-red-700 cursor-pointer'>Delete account</span>
           <span className='text-red-700 cursor-pointer'>Sign out</span>
         </div>
+        <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+        <p className='text-green-700 mt-5'>
+        {updateSuccess ? 'User is updated successfully!' : ''}
+      </p>
         </div>
   )
 }
